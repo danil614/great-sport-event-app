b78f03732c5a228f8bafa1379bef3869dc8a0f9c
Danil Fedoseev <fs159@mail.ru>
2023-03-11 15:19:25 +0500

    Начал делать EntityFramework

---
+++ GreatSportEventApp/Entities/SportEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatSportEventApp.Entities
+{
+    public class SportEvent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int LocationId { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime Duration { get; set; }
+
+        [StringLength(maximumLength: 200)]
+        public string Description { get; set; }
+    }
+}
